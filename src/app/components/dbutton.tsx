321112863954878935aa5c4cdf78373aea2d13a7
Guilherme Martins Valente <guimartins.m.v@gmail.com>
2025-07-27 13:07:31 -0300

    change on first page layout

--- src/app/components/dbutton.tsx
+++ src/app/components/dbutton.tsx
@@ -13,7 +13,7 @@ export default function DButton({buttonName, buttonPage}: buttonProps) {
 
     return (
         <>
-            <button className="bg-blue-700/60 hover:bg-blue-800 text-white font-bold text-sm font-medium py-2 px-4 rounded-lg transition-colors duration-200 cursor-pointer"
+            <button className="text-white hover:text-md hover:font-extrabold hover:border-b-blue-800/60 hover:border-b-2 text-sm font-medium text-shadow transition-colors duration-200 cursor-pointer"
              onClick={() => console.log(buttonPage)}>{buttonName}</button>
         </>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,18 +3,18 @@ import DButton from "./components/dbutton";
 export default function Home() {
   return (
       <>
-        <div  className="flex justify-center pt-[15vh] ml-[20vw] font-extrabold text-white flex-col gap-2"
+          <div className="flex gap-6 screen- ml-[42vw] p-4">
+            <DButton  buttonName={"Projects"} buttonPage={"Page"}/>
+            <DButton  buttonName={"Skills"}   buttonPage={"Page"}/>
+            <DButton  buttonName={"Contact"}  buttonPage={"Page"}/>
+            <DButton  buttonName={"About Me"} buttonPage={"Page"}/>
+          </div>
+        <div  className="flex justify-center pt-[15vh] ml-[15vw] font-extrabold text-white flex-col gap-2"
         style={{ 
             textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' 
         }}>
-          <h1 className="text-7xl text-blue-700/60">Welcome!</h1> 
-          <h2>My name is <span className="text-blue-800 text-2xl">Guilherme Valente,</span> </h2>
-          <h2>I'm a <span className="text-blue-800 text-2xl">Software Developer!</span></h2>
-          <div className="flex gap-2">
-            <DButton  buttonName={"Projects"} buttonPage={"Page"}/>
-            <DButton  buttonName={"Skills"} buttonPage={"Page"}/>
-            <DButton  buttonName={"Contact"} buttonPage={"Page"}/>
-          </div>
+          <h2 className="text-white text-2xl">Hi, I'm Guilherme!</h2>
+          <h1 className="text-7xl text-blue-700/60">Web Developer</h1> 
         </div>
       </>
   );
